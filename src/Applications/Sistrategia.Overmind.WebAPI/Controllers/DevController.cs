653c30333a84a7ec005d43d85c228b7ed9e19020
Ernesto Ocampo <ernesto@sistrategia.com>
2026-07-15 14:00:03 -0600

    Add configuration and controller files
    - Create .hintrc for hinting rules
    - Add launch.json for .NET Core debugging configuration
    - Add tasks.json for build, publish, and watch tasks
    - Create .editorconfig for code style settings
    - Implement DevController for database management actions
    - Add omnisharp.json for formatting options

---
+++ src/Applications/Sistrategia.Overmind.WebAPI/Controllers/DevController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+// using Sistrategia.Data;
+// using Sistrategia.Overmind.Data.SqlClient.Migrations;
+// using Sistrategia.Overmind.WebAPI.Models;
+// using Sistrategia.Overmind.WebAPI.Services;
+// using Sistrategia.Overmind.WebAPI.Utils;
+// using Sistrategia.Security;
+
+namespace Sistrategia.Overmind.WebAPI.Controllers;
+
+[ApiController, Route("api/dev")]
+[Authorize(Roles = "Developer")]
+public class DevController : ControllerBase
+{
+    private readonly ILogger<DevController> dbLogger;
+
+    public DevController(
+        // IDatabaseManager databaseManager,
+        // IMigrationRunner migrationRunner,
+        // IEmailNotificationService emailService, 
+        ILogger<DevController> logger) {
+        // DatabaseManager = databaseManager;
+        // MigrationRunner = migrationRunner;
+        // EmailService = emailService;
+        dbLogger = logger;
+    }
+
+    [HttpPost("createdatabase")]
+    public ActionResult CreateDatabase() {
+        dbLogger.LogInformation("Creating a new database...");
+        // DatabaseManager.CreateDatabase();
+        dbLogger.LogInformation("New database created.");
+        return Created();
+    }
+
+    [HttpPost("createschema")]
+    public ActionResult CreateSchema() {
+        dbLogger.LogInformation("Creating the database schema...");
+        // DatabaseManager.CreateSchema();
+        dbLogger.LogInformation("Database schema created.");
+        return Created();
+    }
+
+    [HttpPost("dropschema")]
+    public ActionResult DropSchema() {
+        dbLogger.LogInformation("Droping the database schema...");
+        // DatabaseManager.DropSchema();
+        dbLogger.LogInformation("Database schema dropped.");
+        return Ok();
+    }
+}
